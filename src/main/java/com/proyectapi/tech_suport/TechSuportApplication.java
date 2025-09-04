cbc954904db42aefd6c33cea5472aec3ad4307f2
Paula Apsé <89433554+Kenhya12@users.noreply.github.com>
2025-09-04 18:50:36 +0200

    first commit

---
+++ src/main/java/com/proyectapi/tech_suport/TechSuportApplication.java
@@ -0,0 +1,13 @@
+package com.proyectapi.tech_suport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSuportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechSuportApplication.class, args);
+	}
+
+}
